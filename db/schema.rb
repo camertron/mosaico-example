74302c0262f9e03cebbf537ba8955bd15f999390
Cameron Dutro <camertron@gmail.com>
2018-02-11 16:34:18 -0800

    Initial commit

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170824233755) do
+
+  create_table "mosaico_images", force: :cascade do |t|
+    t.string   "file",       null: false
+    t.integer  "width",      null: false
+    t.integer  "height",     null: false
+    t.integer  "filesize",   null: false
+    t.string   "mime_type",  null: false
+    t.string   "type",       null: false
+    t.integer  "parent_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "mosaico_projects", force: :cascade do |t|
+    t.text     "html"
+    t.text     "content"
+    t.text     "metadata"
+    t.string   "template_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
